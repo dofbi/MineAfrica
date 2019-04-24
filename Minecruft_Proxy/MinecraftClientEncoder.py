e5a6d666d0a856796caa0c706074b033d079c6c6
Bachlava <nathan.tusing95@gmail.com>
2019-04-24 13:50:24 -0400

    Style Changes

--- Minecruft_Proxy/MinecraftClientEncoder.py
+++ Minecruft_Proxy/MinecraftClientEncoder.py
@@ -1,223 +1,201 @@
 #!/usr/bin/python3
+#Author: Nathan Tusing
+#
+"""
+Python 3 module with core classes for establishing and maintaing
+Minecraft Client connections, encoding encrypted byte streams, and
+factory class as an interface to build different types of Minecraft
+Class Encoders.
+"""
 
-import multiprocessing
-from twisted.internet import reactor, defer
-from twisted.internet.endpoints import TCP4ClientEndpoint
 from quarry.net.client import ClientFactory, SpawningClientProtocol
-from quarry.net.auth import ProfileCLI
 from Crypto.Cipher import AES
 
-#This class handles encoding/decoding byte data to/from
-#minecraft network packets. Uses a Reactor Design pattern
-#for handling asynchronous packet delivery/receiving
-
 class MinecraftClientEncoder(SpawningClientProtocol):
-	def __init__(self, factory, remote_addr):
-		self.packet_done = False
-		self.AES_Block_Len = AES.block_size
-		self.out_enc_buff = bytearray() 
-		self.in_enc_buff = bytearray()
-		super(MinecraftClientEncoder, self).__init__(factory, remote_addr)
+    """
+    The MinecraftClient Encoder is a class that represents a Minecraft Client connection and performs the FTE encoding of an encrypted bit stream.
+    """
+    def __init__(self, factory, remote_addr):
+        self.packet_done = False
+        self.AES_Block_Len = AES.block_size
+        self.out_enc_buff = bytearray()
+        self.in_enc_buff = bytearray()
+        super(MinecraftClientEncoder, self).__init__(factory, remote_addr)
 
-	def update_player_full(self): 
-		self.send_packet("player_position", self.buff_type.pack("ddd?", self.pos_look[0], self.pos_look[1], self.pos_look[2], True))
+    def update_player_full(self):
+        """
+        Sends a player's position to the server every 20 ticks (1 second).        """
+        self.send_packet("player_position", self.buff_type.pack("ddd?", self.pos_look[0], self.pos_look[1], self.pos_look[2], True))
 
-#Encoding Functions*************************************************************** 
-	def get_byte_from_buff(self, buff): 
-		if(len(buff) == 0): 
-			buff_byte = 256; 
-		else:
-			buff_byte = buff.pop(0)
-		return buff_byte
+#Encoding Functions***************************************************************
+    def get_byte_from_buff(self, buff):
+        if len(buff) == 0:
+            buff_byte = 256
+        else:
+            buff_byte = buff.pop(0)
+        return buff_byte
 
-	def get_bytes_from_buff(self, buff, num_bytes): 
-		buff_bytes = []
-		for num in range(0, num_bytes):
-			buff_byte = self.get_byte_from_buff(buff)
-			buff_bytes.append(buff_byte)
-		return buff_bytes
+    def get_bytes_from_buff(self, buff, num_bytes):
+        buff_bytes = []
+        for num in range(0, num_bytes):
+            buff_byte = self.get_byte_from_buff(buff)
+            buff_bytes.append(buff_byte)
+        return buff_bytes
 
-	def check_buff(self, buff):
-		if len(buff) < 1 and self.factory.forwarding_packet_queue.qsize() > 0: 
-			data = self.factory.forwarding_packet_queue.get()
-			if(data != None): 
-				buff = bytearray(data)
-			self.encode_player_look()
-		return buff
+    def check_buff(self, buff):
+        if len(buff) < 1 and self.factory.forwarding_packet_queue.qsize() > 0:
+            data = self.factory.forwarding_packet_queue.get()
+            if data != None:
+                buff = bytearray(data)
+            self.encode_player_look()
+        return buff
 
-	#This is the primary encoding method, it checks for data in the packet queue
-	#and then turns the packets into Minecraft movements
-	def encode(self): 
-		if self.factory.forwarding_packet_queue.qsize() > 0 or len(self.out_enc_buff) > 0:
-			if len(self.out_enc_buff) > 0:
-				for i in range(36, 44): 
-					self.encode_inventory_action(i)
-				#self.encode_player_position_and_look()
-			
-			self.out_enc_buff = self.check_buff(self.out_enc_buff)
+    #This is the primary encoding method, it checks for data in the packet queue
+    #and then turns the packets into Minecraft movements
+    def encode(self):
+        if self.factory.forwarding_packet_queue.qsize() > 0 or len(self.out_enc_buff) > 0:
+            if len(self.out_enc_buff) > 0:
+                for i in range(36, 44):
+                    self.encode_inventory_action(i)
+                #self.encode_player_position_and_look()
 
-			
+            self.out_enc_buff = self.check_buff(self.out_enc_buff)
 
-	def encode_inventory_action(self, slot_num):
-		item_id = int(self.get_byte_from_buff(self.out_enc_buff))
-		#self.logger.info(item_id)
-		self.send_packet("creative_inventory_action", self.buff_type.pack('h',slot_num) + self.buff_type.pack_slot(item_id))
-	
-	#These movement methods rely on player positional data which was received from the server
-	def encode_player_look(self):
-		look_yaw = self.pos_look[3]
-		look_pitch = self.pos_look[4]
-		on_ground =	True 
-		
-		self.send_packet("player_look", self.buff_type.pack( 'ff?', look_yaw, look_pitch, on_ground))
 
-	def encode_player_position(self):
-		pos_x = self.pos_look[0]
-		pos_y = self.pos_look[1]
-		pos_z = self.pos_look[2]
-		on_ground =	True 
 
-		self.send_packet("player_position", self.buff_type.pack('ddd?', pos_x, pos_y, pos_z, on_ground))
+    def encode_inventory_action(self, slot_num):
+        item_id = int(self.get_byte_from_buff(self.out_enc_buff))
+        #self.logger.info(item_id)
+        self.send_packet("creative_inventory_action", self.buff_type.pack('h',slot_num) + self.buff_type.pack_slot(item_id))
 
-	def encode_player_position_and_look(self): 
-		out_bytes = self.get_bytes_from_buff(self.out_enc_buff, 2)
-		
-		#x_offset = int(out_bytes[0])
-		#z_offset = int(out_bytes[1])
-		yaw = int(out_bytes[0])
-		pitch = int(out_bytes[1])
-		#print(out_bytes)
+    #These movement methods rely on player positional data which was received from the server
+    def encode_player_look(self):
+        look_yaw = self.pos_look[3]
+        look_pitch = self.pos_look[4]
+        on_ground = True
 
-		pos_x = self.pos_look[0] + 0.1 #x_offset/128.0 - 1.0
-		pos_y = self.pos_look[1]
-		pos_z = self.pos_look[2] + 0.1 #z_offset/128.0 - 1.0
-		look_yaw = float(yaw * 1.0)
-		look_pitch = float(pitch * 1.0) 
-		on_ground =	True 
+        self.send_packet("player_look", self.buff_type.pack('ff?', look_yaw, look_pitch, on_ground))
 
-		self.send_packet("player_position_and_look", self.buff_type.pack( 'dddff?', pos_x, pos_y, pos_z, look_yaw, look_pitch, True))
-	
+    def encode_player_position(self):
+        pos_x = self.pos_look[0]
+        pos_y = self.pos_look[1]
+        pos_z = self.pos_look[2]
+        on_ground = True
 
-	#def encode_place_and_remove_block(self):
+        self.send_packet("player_position", self.buff_type.pack('ddd?', pos_x, pos_y, pos_z, on_ground))
 
-	#This is when we set our hooks for the encode event to check for new web requests. 
-	#def packet_login_success(self, buff):
-		#super(MinecraftClientEncoder,self).packet_login_success(buff)
+    def encode_player_position_and_look(self):
+        out_bytes = self.get_bytes_from_buff(self.out_enc_buff, 2)
 
-	#This method needed to be overloaded from the Spawning client protocol class
-	#because it added unessary event loops
-	def packet_player_position_and_look(self, buff):
-		p_pos_look = buff.unpack('dddff')
+        #x_offset = int(out_bytes[0])
+        #z_offset = int(out_bytes[1])
+        yaw = int(out_bytes[0])
+        pitch = int(out_bytes[1])
+    
+        pos_x = self.pos_look[0] + 0.1 #x_offset/128.0 - 1.0
+        pos_y = self.pos_look[1]
+        pos_z = self.pos_look[2] + 0.1 #z_offset/128.0 - 1.0
+        look_yaw = float(yaw * 1.0)
+        look_pitch = float(pitch * 1.0)
+        on_ground =    True
 
-		# 1.7.x
-		if self.protocol_version <= 5:
-			p_on_ground = buff.unpack('?')
-			self.pos_look = p_pos_look
+        self.send_packet("player_position_and_look", self.buff_type.pack('dddff?', pos_x, pos_y, pos_z, look_yaw, look_pitch, True))
 
-		# 1.8.x
-		else:
-			p_flags = buff.unpack('B')
 
-		for i in range(5):
-			if p_flags & (1 << i):
-				self.pos_look[i] += p_pos_look[i]
-			else:
-				self.pos_look[i] = p_pos_look[i]
+    #def encode_place_and_remove_block(self):
 
-		# 1.9.x
-		if self.protocol_version > 47:
-			teleport_id = buff.unpack_varint()
+    #This is when we set our hooks for the encode event to check for new web requests.
+    #def packet_login_success(self, buff):
+        #super(MinecraftClientEncoder,self).packet_login_success(buff)
 
-		# Send Player Position And Look
+    #This method needed to be overloaded from the Spawning client protocol class
+    #because it added unessary event loops
+    def packet_player_position_and_look(self, buff):
+        p_pos_look = buff.unpack('dddff')
 
-		# 1.7.x
-		if self.protocol_version <= 5:
-			self.send_packet("player_position_and_look", self.buff_type.pack( 'ddddff?', self.pos_look[0], self.pos_look[1] - 1.62, self.pos_look[1], self.pos_look[2], self.pos_look[3], self.pos_look[4], True))
+        # 1.7.x
+        if self.protocol_version <= 5:
+            p_on_ground = buff.unpack('?')
+            self.pos_look = p_pos_look
 
-		# 1.8.x
-		elif self.protocol_version <= 47:
-			self.send_packet("player_position_and_look", self.buff_type.pack( 'dddff?', self.pos_look[0], self.pos_look[1], self.pos_look[2], self.pos_look[3], self.pos_look[4], True)) 
-						        
+        # 1.8.x
+        else:
+            p_flags = buff.unpack('B')
+
+        for i in range(5):
+            if p_flags & (1 << i):
+                self.pos_look[i] += p_pos_look[i]
+            else:
+                self.pos_look[i] = p_pos_look[i]
+
         # 1.9.x
-		else:
-			self.send_packet("teleport_confirm", self.buff_type.pack_varint( teleport_id))
+        if self.protocol_version > 47:
+            teleport_id = buff.unpack_varint()
 
-		if not self.spawned:
-			self.ticker.add_loop(1,self.encode)
-			self.ticker.add_loop(20, self.update_player_full)
-			self.spawned = True
+        # Send Player Position And Look
+
+        # 1.7.x
+        if self.protocol_version <= 5:
+            self.send_packet("player_position_and_look", self.buff_type.pack('ddddff?', self.pos_look[0], self.pos_look[1] - 1.62, self.pos_look[1], self.pos_look[2], self.pos_look[3], self.pos_look[4], True))
+
+        # 1.8.x
+        elif self.protocol_version <= 47:
+            self.send_packet("player_position_and_look", self.buff_type.pack('dddff?', self.pos_look[0], self.pos_look[1], self.pos_look[2], self.pos_look[3], self.pos_look[4], True))
+
+        # 1.9.x
+        else:
+            self.send_packet("teleport_confirm", self.buff_type.pack_varint(teleport_id))
+
+        if not self.spawned:
+            self.ticker.add_loop(1, self.encode)
+            self.ticker.add_loop(20, self.update_player_full)
+            self.spawned = True
 
 #Decoding Functions***********************************************************************************
-	#The decoding packet handlers fire whenever a new packet is sent by the server and
-	#add each received TCP packet to the packet handler
-	def update_incoming_buffer(self):
-		incoming_q = self.factory.receiving_packet_queue
-		incoming_q.put(self.in_enc_buff)	
-		self.in_enc_buff = bytearray()
+    #The decoding packet handlers fire whenever a new packet is sent by the server and
+    #add each received TCP packet to the packet handler
+    def update_incoming_buffer(self):
+        incoming_q = self.factory.receiving_packet_queue
+        incoming_q.put(self.in_enc_buff)
+        self.in_enc_buff = bytearray()
 
-	def check_entity(self, data): 
-		return data >= 20000; 
+    def check_entity(self, data):
+        return data >= 20000
 
-	def packet_entity_head_look(self, buff): 
-		entity_data = buff.unpack_varint()
-		head_pos = buff.unpack("B")
-		if self.check_entity(entity_data): 
-			self.in_enc_buff.append(head_pos)
-	
-	def packet_spawn_mob(self, buff):
-		buff.read()
-	
-	def packet_entity_relative_move(self, buff):
-		enemy_id = buff.unpack_varint()
-		enemy_pos = buff.unpack("hhh?")
+    def packet_entity_head_look(self, buff):
+        entity_data = buff.unpack_varint()
+        head_pos = buff.unpack("B")
+        if self.check_entity(entity_data):
+            self.in_enc_buff.append(head_pos)
 
-	def packet_entity_look(self, buff):
-		self.update_incoming_buffer()
-		enemy_id = buff.unpack_varint()
-		self.update_incoming_buffer()
-		#Agreed enemy id for sending messages, negotiate with packages later
-		if(enemy_id == 21445): 
-			self.factory.receiving_packet_queue.put(None)
+    def packet_spawn_mob(self, buff):
+        buff.read()
 
-		enemy_look = buff.unpack("bb?")
-		enemy_yaw = enemy_look[0]
-		#print(enemy_yaw)
-		buff.discard()
+    def packet_entity_relative_move(self, buff):
+        enemy_id = buff.unpack_varint()
+        enemy_pos = buff.unpack("hhh?")
 
-	def packet_entity_look_and_relative_move(self, buff):
-		enemy_id = buff.unpack_varint()
-		buff.read()
+    def packet_entity_look(self, buff):
+        self.update_incoming_buffer()
+        enemy_id = buff.unpack_varint()
+        self.update_incoming_buffer()
+        #Agreed enemy id for sending messages, negotiate with packages later
+        if enemy_id == 21445:
+            self.factory.receiving_packet_queue.put(None)
+
+        enemy_look = buff.unpack("bb?")
+        enemy_yaw = enemy_look[0]
+        #print(enemy_yaw)
+        buff.discard()
+
+    def packet_entity_look_and_relative_move(self, buff):
+        enemy_id = buff.unpack_varint()
+        buff.read()
 
 
 class MinecraftEncoderFactory(ClientFactory):
-	protocol = MinecraftClientEncoder
-	def __init__(self, profile = None, f_queue = None, r_queue = None ): 
-		self.forwarding_packet_queue = f_queue
-		self.receiving_packet_queue = r_queue
-		super(MinecraftEncoderFactory, self).__init__(profile)
-
-
-@defer.inlineCallbacks
-def run(args):
-	listy = multiprocessing.Queue()
-	listy.put(bytearray(b'chocolate'))
-	hold = multiprocessing.Queue()
-	# Log in
-	profile = yield ProfileCLI.make_profile(args)
-# Create factory
-	factory = MinecraftEncoderFactory(profile, listy, hold)
-# Connect!
-	factory.connect(args.host, args.port)
-
-def main(argv):
-	parser = ProfileCLI.make_parser()
-	parser.add_argument("host")
-	parser.add_argument("-p", "--port", default=25565, type=int)
-	args = parser.parse_args(argv)
-
-	run(args)
-	reactor.run()
-
-if __name__ == "__main__":
-	import sys
-	main(sys.argv[1:])
+    protocol = MinecraftClientEncoder
+    def __init__(self, profile=None, f_queue=None, r_queue=None):
+        self.forwarding_packet_queue = f_queue
+        self.receiving_packet_queue = r_queue
+        super(MinecraftEncoderFactory, self).__init__(profile)
--- Minecruft_Proxy/MinecraftProxyEncoder.py
+++ Minecruft_Proxy/MinecraftProxyEncoder.py
@@ -7,28 +7,28 @@ from quarry.net.proxy import DownstreamFactory, Upstream, UpstreamFactory, Bridg
 from quarry.types import uuid
 from Crypto.Cipher import AES
 
-#Each client needs its own packet buffer and assigned entity to signal 
+#Each client needs its own packet buffer and assigned entity to signal
 #that it is done
-class UpstreamEncoder(Upstream): 
+class UpstreamEncoder(Upstream):
 	in_enc_buff = bytearray()
 	assigned_enemy = 0
 	assigned_id = 0
 
-class UpstreamEncoderFactory(UpstreamFactory): 
+class UpstreamEncoderFactory(UpstreamFactory):
 	protocol = UpstreamEncoder	
 				
-#Each Client and Proxy needs to keep track of which enemy is their own. 
+#Each Client and Proxy needs to keep track of which enemy is their own.
 class MinecraftProxyBridge(Bridge):
 	quiet_mode = False
 	events_enabled = False
 	upstream_factory_class = UpstreamEncoderFactory
 	
-	def __init__(self, downstream_factory, downstream): 
+	def __init__(self, downstream_factory, downstream):
 		self.clients_and_positions = []
 		self.out_enc_buff = bytearray()
 		self.old_enc_buff = bytearray()
-		self.mobs_per_client = 100 
-		self.first_enemy_id = 30000 
+		self.mobs_per_client = 100
+		self.first_enemy_id = 30000
 		self.packet_done = False
 		self.is_waiting = False
 		self.block_len = AES.block_size
@@ -39,14 +39,14 @@ class MinecraftProxyBridge(Bridge):
 
 		super().__init__(downstream_factory, downstream)
 
-	def get_byte_from_buff(self, buff): 
-		if(len(buff) == 0): 
-			buff_byte = 256; 
+	def get_byte_from_buff(self, buff):
+		if(len(buff) == 0):
+			buff_byte = 256;
 		else:
 			buff_byte = buff.pop(0)
 		return buff_byte
 
-	def get_bytes_from_buff(self, buff, num_bytes): 
+	def get_bytes_from_buff(self, buff, num_bytes):
 		buff_bytes = []
 		for num in range(0, num_bytes):
 			buff_byte = self.get_byte_from_buff(buff)
@@ -54,61 +54,61 @@ class MinecraftProxyBridge(Bridge):
 		return buff_bytes
 
 	def check_buff(self, buff):
-		if len(buff) < 1 and self.downstream.factory.forwarding_packet_queue.qsize() > 0: 
+		if len(buff) < 1 and self.downstream.factory.forwarding_packet_queue.qsize() > 0:
 			#self.logger.info(self.old_enc_buff)
 			#self.logger.info(self.out_enc_buff)
 			data = self.downstream.factory.sync_buff(self.old_enc_buff)
-			if(data != None and data != self.old_enc_buff): 
+			if(data != None and data != self.old_enc_buff):
 				buff = bytearray(data)
 				self.old_enc_buff = self.out_enc_buff
 
 			self.enemy_enc_look()
 		return buff
 
-	def encode(self): 
+	def encode(self):
 		if self.downstream.factory.forwarding_packet_queue.qsize() > 0 or len(self.out_enc_buff) > 0:
-			if len(self.out_enc_buff) > 0: 
+			if len(self.out_enc_buff) > 0:
 				for i in range (self.first_enemy_id, self.first_enemy_id + self.mobs_per_client):
 					self.enemy_enc_head_look(i)
 
 			self.out_enc_buff = self.check_buff(self.out_enc_buff)
 
 
-	def enemy_enc_head_look(self, enemy_id): 
+	def enemy_enc_head_look(self, enemy_id):
 		val = 0
 		yaw = self.get_byte_from_buff(self.out_enc_buff)
-		if(yaw != 256): 
+		if(yaw != 256):
 			self.downstream.send_packet("entity_head_look", self.downstream.buff_type.pack_varint(enemy_id), self.downstream.buff_type.pack("B", yaw ))
 
 	#Uses os.urandom under the hood to generate secure numbers
-	def gen_rand(self, bound): 
+	def gen_rand(self, bound):
 		rand_gen = random.SystemRandom()
 		return rand_gen.randint(0, bound)
 
-	def enemy_enc_look(self): 
-		mid = self.first_enemy_id 
-		yaw = self.gen_rand(255)  
+	def enemy_enc_look(self):
+		mid = self.first_enemy_id
+		yaw = self.gen_rand(255)
 		val = self.gen_rand(255)
 		self.downstream.send_packet("entity_look", self.downstream.buff_type.pack_varint(mid), self.downstream.buff_type.pack("BB?", yaw, val, True ))
 	
-	def spawn_mobs(self, player_position): 
+	def spawn_mobs(self, player_position):
 		first_mob_id = self.first_enemy_id
 		num_mobs = self.mobs_per_client
 		position = player_position
 
-		for i in range(0, num_mobs): 
+		for i in range(0, num_mobs):
 			chosen_client = self.downstream
-			x_pos = position[0] + self.gen_rand(255000)/1000.0 - 127 
-			y_pos = position[1]  
-			z_pos = position[2] + self.gen_rand(255000)/1000.0 - 127 
-			yaw = self.gen_rand(255) 
-			pitch = 0 
-			head_pitch = self.gen_rand(255) 
-			velocity_x = 0 
+			x_pos = position[0] + self.gen_rand(255000)/1000.0 - 127
+			y_pos = position[1]
+			z_pos = position[2] + self.gen_rand(255000)/1000.0 - 127
+			yaw = self.gen_rand(255)
+			pitch = 0
+			head_pitch = self.gen_rand(255)
+			velocity_x = 0
 			velocity_y = 0
-			velocity_z = 0 
-			enemy_id = first_mob_id + i 
-			enemy_type =  50  + self.gen_rand(2) 
+			velocity_z = 0
+			enemy_id = first_mob_id + i
+			enemy_type = 50 + self.gen_rand(2)
 			meta_data = 255 #signals that no metadata exists
 
 			chosen_client.send_packet("spawn_mob",chosen_client.buff_type.pack_varint(enemy_id) + chosen_client.buff_type.pack_uuid(uuid.UUID.random()) + self.downstream.buff_type.pack_varint(enemy_type) + chosen_client.buff_type.pack("dddBBBhhhB", x_pos, y_pos, z_pos, yaw, pitch, head_pitch, velocity_x, velocity_y, velocity_z, meta_data))
@@ -120,7 +120,7 @@ class MinecraftProxyBridge(Bridge):
 #--------------------------------------------------------------------
 	def update_incoming_buffer(self, stream):
 		did_add_buff = False
-		if(len(stream.in_enc_buff) > 0 and len(stream.in_enc_buff) % AES.block_size == 0): 
+		if(len(stream.in_enc_buff) > 0 and len(stream.in_enc_buff) % AES.block_size == 0):
 			self.downstream_factory.receiving_packet_queue.put(stream.in_enc_buff)
 			did_add_buff = True
 		stream.in_enc_buff = bytearray()
@@ -131,15 +131,15 @@ class MinecraftProxyBridge(Bridge):
 	#def packet_downstream_spawn_mob(self, buff):
 		#buff.discard()
 
-	#def packet_downstream_entity_relative_move(self, buff): 
+	#def packet_downstream_entity_relative_move(self, buff):
 		#buff.discard()
 
-	def packet_upstream_creative_inventory_action(self, buff): 
+	def packet_upstream_creative_inventory_action(self, buff):
 		buff.save()
 		buff.unpack("h")
-		slot_num  = buff.unpack_slot()
+		slot_num = buff.unpack_slot()
 		item_num = slot_num["item"]
-		if(item_num != None and item_num < 256): 
+		if(item_num != None and item_num < 256):
 			self.upstream.in_enc_buff.append(item_num)
 
 		buff.restore()
@@ -147,19 +147,19 @@ class MinecraftProxyBridge(Bridge):
 
 	#When the player sends just a look command, that means the packet
 	#is finished
-	def packet_upstream_player_look(self, buff): 
+	def packet_upstream_player_look(self, buff):
 		buff.save()
-		if(self.update_incoming_buffer(self.upstream)): 
+		if(self.update_incoming_buffer(self.upstream)):
 			self.downstream_factory.receiving_packet_queue.put(None)
 		buff.restore()
 		self.upstream.send_packet("player_look", buff.read())
 
-	def packet_upstream_player_position(self, buff): 
+	def packet_upstream_player_position(self, buff):
 		buff.save()
 		buff.restore()
 		self.upstream.send_packet("player_position", buff.read())
 
-	def packet_upstream_player_position_and_look(self, buff): 
+	def packet_upstream_player_position_and_look(self, buff):
 		buff.save()
 
 		pos_x = buff.unpack("d")
@@ -170,7 +170,7 @@ class MinecraftProxyBridge(Bridge):
 		#pos_y = self.pos_look[1]
 		#pos_z = self.pos_look[2] + z_offset/128.0 - 1.0
 	
-		yaw = int(buff.unpack("f")) 
+		yaw = int(buff.unpack("f"))
 		pitch = int(buff.unpack("f"))
 		
 		#if yaw < 256 and yaw > 0:
@@ -181,14 +181,14 @@ class MinecraftProxyBridge(Bridge):
 
 		buff.restore()
 		self.upstream.send_packet("player_position_and_look", buff.read())
-	def packet_downstream_entity_head_look(self, buff): 
+	def packet_downstream_entity_head_look(self, buff):
 		buff.discard()
 	
-	def packet_downstream_entity_look(self, buff): 
+	def packet_downstream_entity_look(self, buff):
 		buff.discard()
 
 	#This packet is only sent if the player dies, or if he is joining
-	def packet_downstream_player_position_and_look(self, buff): 
+	def packet_downstream_player_position_and_look(self, buff):
 		buff.save()
 		pos_and_look_struct = buff.unpack("dddff")
 		self.downstream.ticker.add_loop(1, self.encode)
@@ -214,20 +214,20 @@ class MinecraftProxyFactory(DownstreamFactory):
 		self.forwarding_packet_queue = c_p_queue
 		super(MinecraftProxyFactory, self).__init__()
 	
-	def connectionMade(self): 
+	def connectionMade(self):
 		self.num_client_encoders = num_client_encoders + 1
 		return self.connection_made()
 
-	def sync_buff(self, oldbuff): 
-		if(self.forwarding_packet_queue.qsize() > 0): 
-			if(self.num_client_encoders == self.num_waiting_encoders or len(oldbuff) < 1): 
+	def sync_buff(self, oldbuff):
+		if(self.forwarding_packet_queue.qsize() > 0):
+			if(self.num_client_encoders == self.num_waiting_encoders or len(oldbuff) < 1):
 				self.num_waiting_encoders = 0
 				data = self.forwarding_packet_queue.get()
-				if(data != None): 
-					self.out_enc_buff = bytearray(data) 
-			else: 
+				if(data != None):
+					self.out_enc_buff = bytearray(data)
+			else:
 				self.num_waiting_encoders = self.num_waiting_encoders + 1
-		return self.out_enc_buff 
+		return self.out_enc_buff
 			
 			
 
@@ -246,7 +246,7 @@ def main(argv):
 	melist2 = multiprocessing.Queue()
 	factory = MinecraftProxyFactory(melist, melist2)
 	factory.online_mode = False
-	factory.force_protocol_version = 340 
+	factory.force_protocol_version = 340
 	factory.connect_host = args.connect_host
 	factory.connect_port = args.connect_port
 
